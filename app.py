9623d3216d20e37d98bc2f8bce850482050a5fee
Ray <rctay89@gmail.com>
2020-11-30 16:12:43 +0000

    Append new signups, instead of replacing whole file

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def signup_page(title='Signup Page'):
                 request.form['password'] == request.form['password_confirm']:
             user_data = dict(request.form)
 
-            with open(f'{root}/{directory}/{filename}', mode='w') as employee_file:
+            with open(f'{root}/{directory}/{filename}', mode='a') as employee_file:
                 employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                 employee_writer.writerow([user_data['username'], user_data['email'], user_data['password']])
 
